d007800e289395f4b7c2a8b77cf432024bca35ec
Reynaldi Chernando <reynaldichernando@gmail.com>
2024-11-23 16:14:25 +0700

    Initial commit

---
+++ origins.ts
@@ -0,0 +1,22 @@
+export const registeredOrigins: Record<string, Set<string>> = {
+  "https://corsfix.com": new Set(["cloudflare.com"]),
+  "https://r34.app": new Set([
+    "rule34.xxx",
+    "gelbooru.com",
+    "rule34.paheal.net",
+    "danbooru.donmai.us",
+    "e621.net",
+    "e926.net",
+    "safebooru.org",
+    "realbooru.com",
+    "tbib.org",
+    "xbooru.com",
+    "yande.re",
+    "konachan.com",
+    "hypnohub.net",
+    "aibooru.online",
+    "e6ai.net",
+    "booru.allthefallen.moe",
+    "sakugabooru.com",
+  ]),
+};
