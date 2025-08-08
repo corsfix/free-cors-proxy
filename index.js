638ac1d2413d5b5a3cecbcae357344501a37bc6a
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-08-08 17:53:06 +0700

    Cleanup demo apps

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 module.exports.registeredOrigins = {
-  "https://backtrackhq.web.app": new Set([
-    "youtube.com",
-    "ytimg.com",
-    "duckduckgo.com",
-    "googlevideo.com",
-  ]),
-  "https://periscope.corsfix.com": new Set(["*"]),
-  "https://restfox1.pages.dev": new Set(["*"]),
   "https://html5test.co": new Set(["html5test.eu5.net"]),
   "https://sreesri4.github.io": new Set(["scanner.tradingview.com"]),
 };
